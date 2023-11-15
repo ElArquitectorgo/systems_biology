16442477e2cf78f875b2cbed799660c7e6dd219d
PabloB2G <pablobermudezgamez@uma.es>
2023-11-15 12:16:11 +0100

    Arreglo del setup y launch

--- code/launch.sh
+++ code/launch.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+# Se redirige la mayorías de las salidas estandar para dejar más limpia la consola
+
+echo "Descargando las dependencias necesarias"
+
 # Descargar las dependencias
-bash setup.sh
+bash setup.sh 1> /dev/null
+
+echo "Obteniendo la red de interraccion a traves de StringDB"
 
 # Obtener la red de interacción
-python3 network_obtention.py
+python3 network_obtention.py 1> /dev/null
+
+echo "Procesando la red con igraph"
 
 # Cargamos la red en R y devolvemos los genes de interes
-Rscript network_evaluation.R
+Rscript network_evaluation.R 1> /dev/null
--- code/setup.sh
+++ code/setup.sh
@@ -5,10 +5,11 @@ mkdir -p ../software
 mkdir -p ../software/deps_Python
 mkdir -p ../software/deps_R
 
+# Limiamos la cache de pip para evitarnos problemas
+python3 -m pip cache purge
+
 # Instalar dependencias desde el archivo requirements_python.txt
-pip install -r ../results/requirements_python.txt --target=../software/deps_Python
+python3 -m pip install -r ../results/requirements_python.txt --target=../software/deps_Python 1> /dev/null
 
 # Instalar dependencias desde el archivo requirements_R.txt
-Rscript -e 'install.packages(readLines("../results/requirements_R.txt"), lib="../software/deps_R")'
-# IMPORTANTE EL BUENO ES EL DE ABAJO, DESCOMENTAR PARA ENTREGA FINAL
-# Rscript -e 'install.packages(readLines("../results/requirements_R.txt"), dependencies = TRUE, lib="../software/deps_R")'
+Rscript -e 'install.packages(readLines("../results/requirements_R.txt"), repos="http://cran.us.r-project.org", dependencies=TRUE, lib="../software/deps_R")' 1> /dev/null
