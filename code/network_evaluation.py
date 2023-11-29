de6d6fc28418968f082bbbb5ae253e7473610c6b
ElArquitectorgo <mega2814s@gmail.com>
2023-11-29 19:10:26 +0100

    Cambiado el color del nodo de interés en la red

--- code/network_evaluation.py
+++ code/network_evaluation.py
@@ -14,10 +14,11 @@ data = pd.read_csv(csv_path)
 g = Graph.TupleList(data.itertuples(index=False),directed=False)
 g = g.simplify()
 
+colors = ["lightgreen" if name == "GNB3" else "#FF6961" for name in g.vs["name"]]
 
 # Guardar el grafo en formato SVG
 plot(g, os.path.join(path_script, '..', 'results', 'network.svg'), vertex_label=g.vs["name"], vertex_label_size=10, vertex_label_color="black",
-     vertex_size=12, vertex_label_dist=2, edge_color="darkgray")
+     vertex_size=12, vertex_label_dist=2, edge_color="darkgray", vertex_color=colors)
 
 # Calcular las comunidades mediante el método "cluster_edge_betweenness"
 ceb = g.community_edge_betweenness()
@@ -25,7 +26,7 @@ cl = ceb.as_clustering()
 
 # Guardar el grafo de comunidades en formato SVG
 plot(cl, os.path.join(path_script, '..', 'results', 'comunidades.svg'), mark_groups=True, vertex_label=g.vs["name"], vertex_label_size=10, vertex_label_color="black",
-     vertex_size=12, vertex_label_dist=2, edge_color="darkgray")
+     vertex_size=12, vertex_label_dist=2, edge_color="darkgray", vertex_color="#FF6961")
 
 # Obtener los nodos en la misma comunidad que nuestro nodo de interés
 for comunidad in (cl):
