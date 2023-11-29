80e1a798c1ead51b94a027b883b6d333a5b09830
vboxuser <vboxuser@Ubuntu.22.04.myguest.virtualbox.org>
2023-11-29 20:24:29 +0100

    functional enrichment update

--- code/functional_enrichment.py
+++ code/functional_enrichment.py
@@ -18,13 +18,19 @@ new_network = stringdb.get_network(identifiers=genes, species = 9606, add_nodes=
 ids = pd.concat([new_network['stringId_A'], new_network['stringId_B']])
 # Obtener los valores únicos
 unique_ids = ids.unique()
-enrichment_df = stringdb.get_enrichment(identifiers=unique_ids, species = 9606)
 
-filtered_df = enrichment_df[enrichment_df['preferredNames'].str.contains('GNB3')]
-result = filtered_df[['preferredNames', 'description', 'p_value', 'fdr', 'category', 'term']]
+def get_functional_enrichment(genes):
+    enrichment_df = stringdb.get_enrichment(identifiers=genes, species = 9606)
 
+    filtered_df = enrichment_df[enrichment_df['preferredNames'].str.contains('GNB3')]
+    result = filtered_df[['preferredNames', 'description', 'p_value', 'fdr', 'category', 'term']]
+    return result
+
+first_result = get_functional_enrichment(genes)
+second_result = get_functional_enrichment(unique_ids)
 # construimos la ruta del archivo de salida
-output = os.path.join(path_script, f"../results/{sys.argv[2]}")
-
+first_output = os.path.join(path_script, f"../results/{sys.argv[2]}")
+second_output = os.path.join(path_script, f"../results/{sys.argv[3]}")
 # guardamos el DataFrame en un archivo csv
-result.to_csv(output, index=False)
+first_result.to_csv(first_output, index=False)
+second_result.to_csv(second_output, index=False)
