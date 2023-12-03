2e6e2638182a7d1f350be911ef37cfa7bc18ec75
susanfg <susanafernandez@uma.es>
2023-12-03 18:56:18 +0100

    network information

--- code/network_obtention.py
+++ code/network_obtention.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import stringdb
+from igraph import Graph
 
 """
 Obtiene la red de interacciónes de los genes dados.
@@ -18,6 +19,17 @@ genes = pd.read_excel(path_excel)
 # Nos quedamos con los identificadores de los genes
 genes_ids = genes['GENE_ENTREZ_ID']
 genes_ids = list(map(str, genes_ids))
+
+# Obtener la red de interacciónes de los genes dados
+network_without_gnb3 = stringdb.get_network(identifiers=genes_ids, species=9606)
+edges = [(row['preferredName_A'], row['preferredName_B']) for index, row in network_without_gnb3.iterrows()]
+G = Graph.TupleList(edges, directed=False)
+
+# Almacenar la cantidad de nodos y aristas de la red original
+nodes_edges_count = pd.DataFrame({'Nodes': [len(G.vs)], 'Edges': [len(G.es)]})
+nodes_edges_count.to_csv(os.path.join(path, '..', 'results', 'nodes_edges_count_before_gnb3.csv'), index=False)
+
+
 genes_ids.append("2784")
 
 network = stringdb.get_network(identifiers=genes_ids, species=9606)
